1d45180ad19199c199cb3ed2451d57c1285a6638
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-10 19:04:43 +0100

    add icon

--- App.py
+++ App.py
@@ -4,8 +4,10 @@ from tkinter import *
 class App:
     def __init__(self):
         self.app = Tk()
+
         self.app.title('Youtube video downloader')
         self.app.geometry('800x600')
+        self.app.configure(bg='#f1faee')
 
     def run(self):
         self.app.mainloop()
