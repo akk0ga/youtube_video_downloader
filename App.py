aaefa7d66cba4642233d2d7322fb52632f0f038c
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-10 20:54:53 +0100

    display logo

--- App.py
+++ App.py
@@ -1,15 +1,23 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 
 class App:
     def __init__(self):
         self.app = Tk()
-
+        self.app_width = 800
+        self.app_height = 600
         self.app.title('Youtube video downloader')
         self.app.geometry('800x600')
         self.app.configure(bg='#f1faee')
 
     def run(self):
+        # display logo
+        load = Image.open("img/logo.png")
+        render = ImageTk.PhotoImage(load)
+        img = Label(self.app, border=None, bg='#f1faee', image=render)
+        img.place(x=0, y=0)
+
         self.app.mainloop()
 
 
