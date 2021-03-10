b48be62611ff65604fa9451b0b1b1e6f31483668
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-10 18:46:21 +0100

    add app class and initialize tkinter window

---
+++ App.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+class App:
+    def __init__(self):
+        self.app = Tk()
+        self.app.title('Youtube video downloader')
+
+    def run(self):
+        self.app.mainloop()
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
